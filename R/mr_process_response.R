59c8e40ea7ceac6ce9b4ae7d8d5e358820779a48
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-06-20 00:21:43 +0200

    write special logic for regionalTotals

--- R/mr_process_response.R
+++ R/mr_process_response.R
@@ -1,5 +1,6 @@
 
-mr_process_response <- function(resp, footnotes){
+mr_process_response <- function(resp, footnotes, indicator,verbose){
+  .env <- NULL
   content <- resp |>
     httr2::resp_body_json() |>
     purrr::pluck('Value')
@@ -18,87 +19,105 @@ mr_process_response <- function(resp, footnotes){
 
   writeBin(decoded, con = file_path, useBytes = TRUE)
 
-  data <- mr_process_constant_usd(file_path, footnotes)
+  if(!indicator %in% 'all') {
+    data_to_be_extracted <- xlsx_config |>
+      dplyr::filter(indicator %in% .env$indicator)
+  } else {
+    data_to_be_extracted <- xlsx_config
+  }
+
+
+  suppressWarnings({
+    suppressMessages({
+      data <- purrr::pmap_dfr(
+        data_to_be_extracted,
+        mr_process_xlsx,
+        file_path = file_path,
+        footnotes = footnotes,
+        .progress = verbose
+      ) |>
+        dplyr::select(any_of(c("sheet",
+                               "country",
+                               "region",
+                               "year",
+                               "value",
+                               "missing",
+                               "footnote")))
+    })
+  })
+
 
   unlink(tools::R_user_dir("milRex", which = "data"), recursive = TRUE)
   return(data)
 }
 
 
-
-mr_process_constant_usd <- function(file_path, footnotes){
+mr_process_xlsx <- function(file_path, footnotes, indicator,sheet_name, unit, skip){
   Country <-  content <- content2 <- country <- head <- value <- year <- NULL
 
-  sheet_name <- 'Constant (2022) US$'
-  sheet <- readxl::read_xlsx(file_path,
-                             sheet = sheet_name,
-                             skip = 5) |>
-    tidyr::pivot_longer(cols = -c(Country, 2:3),
-                        names_to = "year",
-                        values_to = "value") |>
-    janitor::clean_names() |>
-    dplyr::select(country, notes, year, value) |>
-    dplyr::mutate(year = as.numeric(year),
-                  missing = dplyr::case_when(
-                    value == "..."~"data unavailable",
-                    value == "xxx"~"country did not exist or was independent"),
-                  value = as.numeric(value)) |>
-    dplyr::filter(!(is.na(value) & is.na(missing)))
 
-  notes <- sheet |> dplyr::pull(notes) |> unique()
 
-  if(footnotes){
-    sheet <- sheet |>
-      dplyr::left_join(mr_get_footnotes(file_path, notes ))
-
-  } else {
-    sheet <- sheet |>
-      dplyr::distinct(country, year, value, missing) |>
+  if(indicator %in% 'regionalTotals'){
+    sheet <- readxl::read_xlsx(file_path,
+                               sheet = sheet_name,
+                               skip = skip,
+                               col_type = "text") |>
+      tidyr::pivot_longer(cols = tidyselect::contains(as.character(1900:2050)),
+                          names_to = "year",
+                          values_to = "value") |>
+      janitor::clean_names() |>
+      dplyr::select(dplyr::any_of(c("region", "notes", "year", "value"))) |>
+      dplyr::mutate(year = year |>
+                      stringr::str_remove("\\.0"),
+                    missing = dplyr::case_when(
+                      value == "..."~"data unavailable",
+                      value == "xxx"~"country did not exist or was independent"),
+                    value = as.numeric(value)) |>
+      dplyr::filter(!(is.na(value) & is.na(missing))) |>
+      dplyr::distinct(region, year, value, missing) |>
+      dplyr::mutate(unit = unit, sheet = sheet_name) |>
       dplyr::mutate(unit = readxl::read_xlsx(file_path,
-                                             sheet = sheet_name,
-                                             col_names = FALSE) |>
+                                             sheet = sheet_name) |>
                       dplyr::pull(1) |>
-                      head(1))
+                      dplyr::nth(unique(unit)))
+  } else {
+    sheet <- readxl::read_xlsx(file_path,
+                               sheet = sheet_name,
+                               skip = skip) |>
+      tidyr::pivot_longer(cols = tidyselect::contains(as.character(1900:2050)),
+                          names_to = "year",
+                          values_to = "value") |>
+      janitor::clean_names() |>
+      dplyr::select(country, notes, year, value) |>
+      dplyr::mutate(year = year |>
+                      stringr::str_remove("\\.0"),
+                    missing = dplyr::case_when(
+        value == "..."~"data unavailable",
+        value == "xxx"~"country did not exist or was independent"),
+        value = as.numeric(value)) |>
+      dplyr::filter(!(is.na(value) & is.na(missing)))
+
+    notes <- sheet |> dplyr::pull(notes) |> unique()
+
+    if(footnotes){
+      sheet <- sheet |>
+        dplyr::left_join(mr_get_footnotes(file_path, notes))
+    }
+
+    sheet <- sheet |>
+      dplyr::distinct(country, year, value, missing,
+                      .keep_all = TRUE) |>
+      dplyr::mutate(unit = unit, sheet = sheet_name) |>
+      dplyr::mutate(unit = readxl::read_xlsx(file_path,
+                                             sheet = sheet_name) |>
+                      dplyr::pull(1) |>
+                      dplyr::nth(unique(unit)))
   }
+
+
+
   return(sheet)
 
 }
 
 
-mr_process_current_usd <- function(file_path, footnotes){
-  Country <-  content <- content2 <- country <- head <- value <- year <- NULL
-
-  sheet_name <- 'Current US$'
-  sheet <- readxl::read_xlsx(file_path,
-                             sheet = sheet_name,
-                             skip = 5) |>
-    tidyr::pivot_longer(cols = -c(Country, 2:3),
-                        names_to = "year",
-                        values_to = "value") |>
-    janitor::clean_names() |>
-    dplyr::select(country, notes, year, value) |>
-    dplyr::mutate(year = as.numeric(year),
-                  missing = dplyr::case_when(
-                    value == "..."~"data unavailable",
-                    value == "xxx"~"country did not exist or was independent"),
-                  value = as.numeric(value)) |>
-    dplyr::filter(!(is.na(value) & is.na(missing)))
-
-  notes <- sheet |> dplyr::pull(notes) |> unique()
-
-  if(footnotes){
-    sheet <- sheet |>
-      dplyr::left_join(mr_get_footnotes(file_path, notes ))
-
-  } else {
-    sheet <- sheet |>
-      dplyr::distinct(country, year, value, missing) |>
-      dplyr::mutate(unit = readxl::read_xlsx(file_path,
-                                             sheet = sheet_name,
-                                             col_names = FALSE) |>
-                      dplyr::pull(1) |>
-                      head(1))
-  }
-  return(sheet)
-
-}
