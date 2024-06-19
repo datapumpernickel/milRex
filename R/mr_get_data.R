d85b08868f83cea89763a1f23268cc64101cce5d
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-06-19 08:54:08 +0200

    first utility functions, only constant USD so far

---
+++ R/mr_get_data.R
@@ -0,0 +1,44 @@
+
+#' Get data from SIPRI Milex database
+#'
+#' @param indicator The type of data to download.
+#' @param verbose Whether to be verbose.
+#' @param footnotes Whether to join footnotes to the results table.
+#' @param cache Whether to cache response from SIPRI and processed data.
+#'
+#' @return A tibble.
+#' @export
+mr_get_data <- function(indicator = "constantUSD",
+                        verbose = FALSE,
+                        footnotes = FALSE,
+                        cache = TRUE){
+  rlang::check_required(indicator)
+
+  rlang::arg_match(
+    indicator,
+    values = c(
+      "regionalTotals",
+      "currencyFY",
+      "currencyCY",
+      "constantUSD",
+      "currentUSD",
+      "shareOfGDP",
+      "perCapita",
+      "shareGovt"
+    )
+  )
+
+  req <- mr_build_request(verbose)
+
+  if(cache){
+    resp <- mr_perform_request_cached(req)
+    data <- mr_process_response_cached(resp, footnotes)
+
+  } else {
+    resp <- mr_perform_request(req)
+    data <- mr_process_response(resp, footnotes)
+  }
+
+  return(data)
+
+}
---
+++ R/zzz.R
@@ -0,0 +1,38 @@
+
+.onLoad <- function(libname, pkgname) {
+  max_size_env <- Sys.getenv("MILREX_CACHE_MAX_SIZE")
+  max_age_env <- Sys.getenv("MILREX_CACHE_MAX_AGE")
+  max_n_env <- Sys.getenv("MILREX_CACHE_MAX_N")
+
+  max_size <- ifelse(nzchar(max_size_env), eval(parse(text = max_size_env)),
+                     1024 * 1024^2)
+  max_age <- ifelse(nzchar(max_age_env), eval(parse(text = max_age_env)), Inf)
+  max_n <- ifelse(nzchar(max_n_env), eval(parse(text = max_n_env)), Inf)
+
+  cache <- cachem::cache_disk(dir = tools::R_user_dir('milRex',
+                                                      which = 'cache'),
+                              max_size = max_size,
+                              max_age = max_age,
+                              max_n = max_n)
+
+  mr_perform_request_cached <- memoise::memoise(mr_perform_request,
+                                               cache = cache)
+
+  assign(x = "mr_perform_request_cached",
+         value = mr_perform_request_cached,
+         envir = rlang::ns_env("milRex"))
+
+  mr_process_response_cached <- memoise::memoise(mr_process_response,
+                                                cache = cache)
+
+  assign(x = "mr_process_response_cached",
+         value = mr_process_response_cached,
+         envir = rlang::ns_env("milRex"))
+
+  assign(x = "cache",
+         value = cache,
+         envir = rlang::ns_env("milRex"))
+
+
+
+}
