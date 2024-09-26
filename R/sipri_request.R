145a15c5e7a510b47fd5c00a28eaefefd2a61804
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-09-26 22:56:47 +0200

    incldue verbose ourput for request

--- R/sipri_request.R
+++ R/sipri_request.R
@@ -4,12 +4,16 @@
 #'
 #' @return An `httr2` request object configured with the specified parameters.
 #' @keywords internal
-sipri_build_request <- function(){
+sipri_build_request <- function(verbose = FALSE){
 
   req <- httr2::request("https://backend.sipri.org/api/p/excel-export") |>
-    httr2::req_headers("Content-Type" = "application/json")|>
+    httr2::req_headers("Content-Type" = "application/json") |>
     httr2::req_body_raw(req_body)
 
+  if(verbose){
+    req <- req |>
+      httr2::req_verbose()
+  }
   return(req)
 }
 
