288bd93a5602f6556084b8cf5f2dc1f01e867cf0
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-09-26 23:11:15 +0200

    calling document() again for new datasets

--- R/data.R
+++ R/data.R
@@ -2,13 +2,12 @@
 #'
 #' ISO3 Country codes and regional codes used by SIPRI for matching to the data.
 #'
-#' @format ## `who`
+#' @format ## `sipri_regions`
 #' A data frame with 7,240 rows and 60 columns:
 #' \describe{
 #'   \item{region}{Region name}
 #'   \item{iso3c}{3 letter ISO country codes}
 #'   \item{country}{Country name}
-#'   ...
 #' }
 #' @source <https://milex.sipri.org/sipri>
 "sipri_regions"
